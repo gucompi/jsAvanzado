dd46205df4803276395ccebdc7c7621117f83a5a
gcompiano <gucompi@gmail.com>
2020-04-26 20:03:04 -0300

    cambio estrategia de renderizado, y agrego loading=lazy para que carguen las imagenes de a poco

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-/*este array será en el futuro una lista heroes*/
-
-[{nombre:"Thor"},{nombre:"Capitan America"},{nombre:"Hulk"}].forEach((heroe)=>{
+fetch('https://shrouded-tor-09389.herokuapp.com/all').then((res)=>{
+    return res.json()
+}).then((json)=>{
+    let stringHTML =""
+    json.forEach((heroe)=>{
+        
+        stringHTML=`${stringHTML}
+        <div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+        <img src="${heroe['images/sm']}" loading="lazy"/>
+        <div class="card-body">
+            <p class="card-text">${heroe.name}</p>
+            <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+            </div>
+            <small class="text-muted">9 mins</small>
+            </div>
+        </div>
+        </div>
+        </div>`
     
-    document.querySelector(".row").innerHTML=`${document.querySelector(".row").innerHTML}
-    <div class="col-md-4">
-    <div class="card mb-4 shadow-sm">
-    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-    <div class="card-body">
-        <p class="card-text">${heroe.nombre}</p>
-        <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-        </div>
-        <small class="text-muted">9 mins</small>
-        </div>
-    </div>
-    </div>
-    </div>`
-
+    })
+    document.querySelector('.row').innerHTML=stringHTML
+    
 })
-
-
 
